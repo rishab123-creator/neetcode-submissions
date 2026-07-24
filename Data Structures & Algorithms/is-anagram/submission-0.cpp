60e8efa5d55d5f0e281d2a0fcf7357b25d59a207
rishab123-creator <rishabjain13112005123@gmail.com>
2026-07-24 11:35:20 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        unordered_map<char,int>freq;
+        for(char c:s){
+            freq[c]++;
+        }
+        for(char c:t){
+            freq[c]--;
+        }
+
+        for(auto&it:freq){
+            if(it.second!=0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
